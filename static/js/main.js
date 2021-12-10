b1f24645030ed5dbe8d1b98a7dd253c07238ede1
Alessandro Freda <61095516+alefreda@users.noreply.github.com>
2021-12-10 12:08:41 +0100

    Typewriter, fixed body style

--- static/js/main.js
+++ static/js/main.js
@@ -2,3 +2,20 @@ var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggl
 var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
   return new bootstrap.Tooltip(tooltipTriggerEl)
 })
+
+var text_type_writer = document.getElementById('type-writer');
+
+var typewriter = new Typewriter(text_type_writer, {
+  loop: true,
+  delay: 75,
+});
+
+typewriter
+  .pauseFor(1000)
+  .typeString('Software engineer')
+  .pauseFor(300)
+  .deleteChars(17)
+  .typeString('Data engineer')
+  .pauseFor(500)
+  .start();
+
